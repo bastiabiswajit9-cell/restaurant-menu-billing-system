f2e3e6805e61eac10d11ec83dba3eff0e1c46bd3
bastiabiswajit9-cell <bastiabiswajit9@gmail.com>
2026-08-28 01:41:27 +0530

    Update restaurant management and billing system.py

--- restaurant management and billing system.py
+++ restaurant management and billing system.py
@@ -23,8 +23,8 @@ menu={
 #greet
 print("welcome to our restaurant")
 print("veg menu")
-print("veg role:Rs60\nmix veg role:Rs70\npaneer 65:Rs185\nveg biriyani:Rs100\nveg chowmin:Rs60\nBaby corn 65:Rs170\npaneer 555:Rs210\nsezwan paneer:Rs220\nveg manchurian:Rs150")
-print("Non veg menu\nNon veg role:Rs60\nchicken pakoda:Rs70\nEgg chop:Rs15\nchicken lollypop:Rs80\nchicken biriyani:Rs120\nchicken chowmin:Rs75\nEgg chowmin:Rs50\ntandori chicken:Rs210\nchicken tandori fry:Rs180\nchicken tikka:Rs190\nchicken leg piece:Rs130")
+print("veg role:  Rs60\nmix veg role: Rs70\npaneer 65:  Rs185\nveg biriyani:  Rs100\nveg chowmin:  Rs60\nBaby corn 65:  Rs170\npaneer 555:  Rs210\nsezwan paneer:  Rs220\nveg manchurian:  Rs150")
+print("Non veg menu\nNon veg role: Rs60\nchicken pakoda:  Rs70\nEgg chop:  Rs15\nchicken lollypop:  Rs80\nchicken biriyani:  Rs120\nchicken chowmin:  Rs75\nEgg chowmin:  Rs50\ntandori chicken:  Rs210\nchicken tandori fry:  Rs180\nchicken tikka:  Rs190\nchicken leg piece:  Rs130")
 
 order_total = 0
 while True :
